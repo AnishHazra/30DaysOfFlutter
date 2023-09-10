53718d03b5ccb116ea53213cb693836c73a688de
AnishHazra <anishhazra108@gmail.com>
2023-09-10 22:37:19 +0530

    day 2 done

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    String name = "Anish Op";
+    double pi = 3.14;
+    // bool isMale = true;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catalog App"),
+      ),
+      body: Center(
+        child: Text("Hello $name $days, value of pi is $pi"),
+      ),
+      drawer: const Drawer(),
+    );
+  }
+}
