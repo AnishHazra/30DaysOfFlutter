add36ca3bd6186a6b3f787cfeb1e4e20a9f5ab78
AnishHazra <anishhazra108@gmail.com>
2023-09-20 18:04:52 +0530

    day8 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:awesomeapp/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -17,7 +18,7 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: Text("Hello $name $days, value of pi is $pi"),
       ),
-      drawer: const Drawer(),
+      drawer: const MyDrawer(),
     );
   }
 }
